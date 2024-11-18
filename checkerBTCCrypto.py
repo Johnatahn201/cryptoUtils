90b540abcd155830ecfc46b3bf3eaacc36d913fe
Johnatahn201 <185831596+Johnatahn201@users.noreply.github.com>
2024-11-19 00:00:00 +0500

    'add file checkerBTCCrypto.py'

---
+++ checkerBTCCrypto.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_crypto_price(crypto):
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids={crypto}&vs_currencies=usd'
+    response = requests.get(url)
+    data = response.json()
+    return data[crypto]['usd']
+
+print(get_crypto_price('bitcoin'))
